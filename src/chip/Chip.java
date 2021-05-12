10c83790d4c4493e7f3f4e83b7e65ccd8371b84e
Brian Glaws <glaws.brian@gmail.com>
2021-05-12 00:26:22 -0400

    progress on decode method

--- src/chip/Chip.java
+++ src/chip/Chip.java
@@ -1,4 +1,4 @@
-package chip;
+
 
 import java.util.Scanner;
 import java.io.File;
@@ -7,11 +7,11 @@ import java.io.FileNotFoundException;
 public class Chip {
 
 	private short opcode;
-	private short[] memory = new short[4096];
-	private short[] V = new short[16];
+	private byte[] memory = new byte[4096];
+	private byte[] V = new byte[16];
 
 	// Index register & Program counter, which can have a value from 0x000 to 0xFFF
-	private short I;
+	private byte I;
 	private short pc;
 
 	// Not sure what memory map is but it goes here
@@ -20,42 +20,40 @@ public class Chip {
 	private byte[] gfx = new byte[64 * 32];
 
 	// timers
-	private short delay_timer;
-	private short sound_timer;
+	private byte delay_timer;
+	private byte sound_timer;
 	
 	// stack and stack pointer
-	private short stack[];
-	private short stackPointer;
+	private byte stack[];
+	private byte stackPointer;
 
 	// hex based keypad
-	private short[] key = new short[16];
+	private byte[] key = new byte[16];
 
 
 
 	// font set for rendering
-	
 	final int FONTSET_SIZE = 0x50;
-	final short[] fontset = {
-		0xF0, 0x90, 0x90, 0x90, 0xF0,		// 0
-		0x20, 0x60, 0x20, 0x20, 0x70,		// 1
-		0xF0, 0x10, 0xF0, 0x80, 0xF0,		// 2
-		0xF0, 0x10, 0xF0, 0x10, 0xF0,		// 3
-		0x90, 0x90, 0xF0, 0x10, 0x10,		// 4
-		0xF0, 0x80, 0xF0, 0x10, 0xF0,		// 5
-		0xF0, 0x80, 0xF0, 0x90, 0xF0,		// 6
-		0xF0, 0x10, 0x20, 0x40, 0x40,		// 7
-		0xF0, 0x90, 0xF0, 0x90, 0xF0,		// 8
-		0xF0, 0x90, 0xF0, 0x10, 0xF0,		// 9
-		0xF0, 0x90, 0xF0, 0x90, 0x90,		// A
-		0xE0, 0x90, 0xE0, 0x90, 0xE0,		// B
-		0xF0, 0x80, 0x80, 0x80, 0xF0,		// C
-		0xE0, 0x90, 0x90, 0x90, 0xE0,		// D
-		0xF0, 0x80, 0xF0, 0x80, 0xF0,		// E
-		0xF0, 0x80, 0xF0, 0x80, 0x80		// F
+	private static byte[] fontset = new byte[]{
+		(byte)0xF0,(byte)0x90,(byte)0x90,(byte)0x90,(byte)0xF0,
+		(byte)0x20,(byte)0x60,(byte)0x20,(byte)0x20,(byte)0x70,
+		(byte)0xF0,(byte)0x10,(byte)0xF0,(byte)0x80,(byte)0xF0,
+		(byte)0xF0,(byte)0x10,(byte)0xF0,(byte)0x10,(byte)0xF0,
+		(byte)0x90,(byte)0x90,(byte)0xF0,(byte)0x10,(byte)0x10,
+		(byte)0xF0,(byte)0x80,(byte)0xF0,(byte)0x10,(byte)0xF0,
+		(byte)0xF0,(byte)0x80,(byte)0xF0,(byte)0x90,(byte)0xF0,
+		(byte)0xF0,(byte)0x10,(byte)0x20,(byte)0x40,(byte)0x40,
+		(byte)0xF0,(byte)0x90,(byte)0xF0,(byte)0x90,(byte)0xF0,
+		(byte)0xF0,(byte)0x90,(byte)0xF0,(byte)0x10,(byte)0xF0,
+		(byte)0xF0,(byte)0x90,(byte)0xF0,(byte)0x90,(byte)0x90,
+		(byte)0xE0,(byte)0x90,(byte)0xE0,(byte)0x90,(byte)0xE0,
+		(byte)0xF0,(byte)0x80,(byte)0x80,(byte)0x80,(byte)0xF0,
+		(byte)0xE0,(byte)0x90,(byte)0x90,(byte)0x90,(byte)0xE0,
+		(byte)0xF0,(byte)0x80,(byte)0xF0,(byte)0x80,(byte)0xF0,
+		(byte)0xF0,(byte)0x80,(byte)0xF0,(byte)0x80,(byte)0x80
 	};
 
-
-
+	// Chip constructor
 	public Chip(String path) throws FileNotFoundException {
 		Scanner scan = new Scanner(new File(path));
 		
@@ -63,16 +61,109 @@ public class Chip {
 			memory[i+ 0x050] = fontset[i];
 		}
 		
-
+		// Rom is read in at 0x200
 		int j = 0x200;
 		while (scan.hasNextByte()) {
 			memory[j] = scan.nextByte();
 			j++;
 		}
+		pc = (byte) 0x200;
 		
 	}
 
-	public static void main(String[] args) {
+	// emulation cycle
+	public void emulateCycle() {
+
+		opcode = fetch();
+		decode(opcode);
+		
+
+	}
+	// fetches opcode
+	public short fetch() {
+		byte high = memory[pc++];
+		byte low = memory[pc++];
+
+		return (short) (high << 8 | low);
+	}
+	// decodes opcode
+	public void decode(short opcode){
+		byte nibble0 = (byte) ((opcode & 0xF000) >> 12);
+		byte nibble1 = (byte) ((opcode & 0x0F00) >> 8);
+		byte nibble2 = (byte) ((opcode & 0x00F0) >> 4);
+		byte nibble3 = (byte) (opcode & 0x000F);
+
+		switch(nibble0) {
+			case 0:
+				//use if statements instead of switch
+
+
+			case 1:
+				pc = (short) (opcode & 0x0FFF);
+				break;
+			case 2:
+				pc = (short) (opcode & 0x0FFF);
+				break;
+			case 3: 
+				if (V[nibble1] == 0x00FF) {
+					pc += 2;
+				}
+				break;
+			case 4:
+				if (V[nibble1] != (opcode & 0x00FF)) {
+					pc += 2;
+				}
+				break;
+			case 5: 
+				if (V[nibble1] == V[nibble2]) {
+					pc += 2;
+				}
+				break;
+			case 6:
+				V[nibble1] = (byte) (opcode & 0x00FF);
+				break;
+			case 7:
+				V[nibble1] += (opcode & 0x00FF);
+				break;
+			case 8:
+				switch(nibble3) {
+					case 0:
+						V[nibble1] = V[nibble2];
+						break;
+					case 1:
+						V[nibble1] = (byte) (V[nibble1] | V[nibble2]);
+						break;
+					case 2:
+						V[nibble1] = (byte) (V[nibble1] & V[nibble2]);
+						break;
+					case 3:
+						V[nibble1] = (byte) (V[nibble1] ^ V[nibble2]);
+						break;
+					case 4:
+
+						break;
+					case 5:
+
+						break;
+					case 6:
+
+						break;
+					case 7:
+
+						break;
+					case 8: //needs to be case E
+
+						break;
+				}
+				
+			case 9:
+
+				break;
+				
+		}
+	}
+
+ 	public static void main(String[] args) {
 		
 		String path = null;
 
@@ -92,8 +183,6 @@ public class Chip {
 
 
 	}
-
-
 
 
 }
